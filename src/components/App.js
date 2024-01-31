c9badaada3280ed06369fc4ee2867433e47cc6be
Poshith24 <hw922262@gmail.com>
2024-01-31 22:56:23 +0530

    React router dom implementation

--- src/components/App.js
+++ src/components/App.js
@@ -2,39 +2,22 @@ import { useState } from 'react';
 import Product from './Product';
 import Cart from './Cart';
 import React from 'react';
+import {Route,Routes,Link} from 'react-router-dom'
 
 export default function App() {
 
-  const [cart, setCart] = useState([]);
-
-  const products = [
-    { name: 'Product 1', price: 100 },
-    { name: 'Product 2', price: 150 },
-  ];
-
-  function addToCart(product) {
-    // console.log(product);
-    // console.log(cart);
-    setCart(prevCart => [...prevCart, product]); 
-  }
-
+   
   return (
     <>
-      <Cart cart={cart} />
-
-
-      {products.map(product => (
-      <Product
-        key={product.name}
-        product={product}
-        onAddToCart={() => {
-          addToCart(product); // update cart state
-          setCart([...cart]); // trigger re-render of Cart
-        }}
-      />
-    ))}
-
-
+    <div>
+      Welcome to react router
+      <Routes>
+       {/* <Route path="/cart/*" element={<Cart />} /> */}
+        <Route path="/" element={<div>Home</div>}/>
+        {/* <Route path="/bout" element={<div>About</div>}></Route> */}
+      </Routes>
+    </div>
+      
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,51 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import { Provider } from 'react-redux'
 import store from './redux/store';
+import {
+    BrowserRouter, Routes,Route,Link
+  } from "react-router-dom";
 
+  
+  
+   
+const Home = ()=>{
+    return (
+        <div>
+            <h1>Hello we are in home!</h1>
+        </div>
+    )
+}
+const About = ()=>{
+    console.log("in about")
+    return (
+        <div>
+            <h1>Hello we are in about!</h1>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
+// document.querySelector('#root').csp = "default-src 'none'; font-src 'self';"
+
+
+
 
 // console.log(<App />);
 // console.log(a+b);
-root.render(
-<Provider store={store}>
+{/* <Provider store={store}>
 <App />
-</Provider>
+</Provider> */}
+root.render(
+<React.StrictMode>
+    <BrowserRouter>
+    <Link to="/">Home</Link> <br/>
+    <Link to="/about">About</Link>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+
+    </Routes>
+    </BrowserRouter>
+</React.StrictMode>
 );
 
